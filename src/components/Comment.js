070e7c18c9353089e788b5fcca11ee7480611766
yetnoidea <yetnoidea@hotmail.com>
2016-08-23 15:27:13 +0800

    use flex box for layout

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,15 +3,15 @@ import React from 'react';
 import { formatTime } from 'es6!../utils/format';
 
 export default ({ comment }) => {
-    const { by, time, text } = comment;
+    const { by, time, text, deleted } = comment;
 
     return (
         <div className="comment">
             <div className="comment-meta">
-                <span className="author">{by}</span>
+                <span className="author">{by || '[someone]'}</span>
                 <span className="time">{formatTime(time * 1000)}</span>
             </div>
-            <div className="comment-text" dangerouslySetInnerHTML={{ __html: text }}></div>
+            <div className="comment-text" dangerouslySetInnerHTML={{ __html: (deleted ? '[deleted]' : text) }}></div>
         </div>
     );
 };
